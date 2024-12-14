1d205f60635a2e28b5b6109b45b22a373b124d32
yaroslavTsebro <yaroslav.cebro@gmail.com>
2024-12-14 20:11:04 +0200

    * added fastify

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,18 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000;
 
-  app.useGlobalPipes(
-    new ValidationPipe({ transform: true })
-  );
+  const PORT = configService.get<number>('PORT') || 3000;
+  const HOST = configService.get<string>('HOST') || 'localhost';
 
-  console.log(`Application is running on: http://localhost:${port}`);
-  await app.listen(port);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  console.log(`Application is running on: http://${HOST}:${PORT}`);
+  await app.listen(PORT, HOST);
 }
 bootstrap();
